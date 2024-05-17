54037c7daf65942ebd0ef6a96eece9f41ada50e6
LYM <409940970@qq.com>
2024-05-17 17:29:36 +0800

    添加项目

---
+++ Blasters/Source/Blasters/BlastersGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BlastersGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTERS_API ABlastersGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
