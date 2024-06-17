a3e4d89ed44b15208f7aaa7589e282aa1765a5e1
LYM <409940970@qq.com>
2024-06-17 14:35:59 +0800

    绘制准星ui

--- Blasters/Source/Blasters/Private/HUD/BlasterHUD.cpp
+++ Blasters/Source/Blasters/Private/HUD/BlasterHUD.cpp
@@ -6,4 +6,44 @@
 void ABlasterHUD::DrawHUD()
 {
 	Super::DrawHUD();
+
+	FVector2D ViewportSize;
+	if (GEngine)
+	{
+		GEngine->GameViewport->GetViewportSize(ViewportSize);
+		const FVector2D ViewportCenter(ViewportSize.X / 2.f, ViewportSize.Y / 2.f);
+		if (HUDPackage.CrosshairsCenter)
+		{
+			DrawCrosshair(HUDPackage.CrosshairsCenter, ViewportCenter);
+		}
+		if (HUDPackage.CrosshairsLeft)
+		{
+			DrawCrosshair(HUDPackage.CrosshairsLeft, ViewportCenter);
+		}
+		if (HUDPackage.CrosshairsRight)
+		{
+			DrawCrosshair(HUDPackage.CrosshairsRight, ViewportCenter);
+		}
+		if (HUDPackage.CrosshairsTop)
+		{
+			DrawCrosshair(HUDPackage.CrosshairsTop, ViewportCenter);
+		}
+		if (HUDPackage.CrosshairsBottom)
+		{
+			DrawCrosshair(HUDPackage.CrosshairsBottom, ViewportCenter);
+		}
+
+	}
 }
+
+void ABlasterHUD::DrawCrosshair(UTexture2D* Texture, FVector2D ViewportCenter)
+{
+	const float TextureWidth = Texture->GetSizeX();
+	const float TextureHight = Texture->GetSizeY();
+	//texture中心点位置
+	const FVector2D TextureDrawPoint(ViewportCenter.X - (TextureWidth / 2.f), ViewportCenter.Y - (TextureHight / 2.f));
+
+	DrawTexture(Texture, TextureDrawPoint.X, TextureDrawPoint.Y, TextureWidth, TextureHight, 0.f, 0.f, 1.f, 1.f, FLinearColor::White);
+
+}
+ 
--- Blasters/Source/Blasters/Public/HUD/BlasterHUD.h
+++ Blasters/Source/Blasters/Public/HUD/BlasterHUD.h
@@ -32,6 +32,9 @@ public:
 private:
 	FHUDPackage HUDPackage;
 
+	void DrawCrosshair(UTexture2D* Texture, FVector2D ViewportCenter);
+
+
 public:
 	FORCEINLINE void SetHUDPackage(const FHUDPackage& Package) { HUDPackage = Package; }
 };
